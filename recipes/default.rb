1a5e3fe76101a3c8fbbb5c7780f9377e2c5f0c85
Cody Poll <CJPoll@gmail.com>
2013-09-20 13:34:16 -0600

    Should copy dotfiles now

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+file_owner = node['devenvset']['owner']
+file_group = node['devenvset']['group']
+
+file ".bashrc", file_owner, file_group
+file ".vimrc", file_owner, file_group
+file ".tmux.conf", file_owner, file_group
+
+def file filename, file_owner, firle_group
+	cookbook_file "~/#{filename}" do
+		source filename
+		mode 0755
+		owner file_owner
+		group file_group
+	end
+end
