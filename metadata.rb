7979ee0326b53e83fedb5e6c30aea957a1a97472
Cody Poll <CJPoll@gmail.com>
2013-09-22 00:57:35 -0600

    No - can't do it.

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'cjpoll@gmail.com>'
 license          'All rights reserved'
 description      'Installs/Configures devenvset'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.13'
+version          '0.1.14'
 
 depends "build-essential"
 depends "cmake"
-depends "vim_chef", git: "https://github.com/CJPoll/vim_chef.git"
+depends "vim_chef"
