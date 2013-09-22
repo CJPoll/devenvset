05c9867bd723fe07677c5346923cf7c8f8ea446d
Cody Poll <CJPoll@gmail.com>
2013-09-22 00:07:37 -0600

    Added build-essential and cmake

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,8 @@ maintainer_email 'cjpoll@gmail.com>'
 license          'All rights reserved'
 description      'Installs/Configures devenvset'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.11'
+version          '0.1.12'
 
+depends "build-essential"
+depends "cmake"
 depends "vim_chef"
