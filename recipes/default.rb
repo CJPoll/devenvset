6cff97f767d0bb2fdfa5ac9a5ed2583423f88679
Cody Poll <CJPoll@gmail.com>
2013-09-21 21:59:22 -0600

    added .bash_profile to files; configures git; configures tmux & vim

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
 include_recipe "vim_chef::source"
 
 def move_file filename, file_owner, file_group
@@ -19,7 +20,71 @@ end
 
 file_owner = node['devenvset']['owner']
 file_group = node['devenvset']['group']
+git_name   = node['devenvset']['git']['username']
+git_email  = node['devenvset']['git']['email']
+git_cache_creds = node['devenvset']['git']['cache_credentials']
 
+move_file ".bash_profile", file_owner, file_group
 move_file ".bashrc", file_owner, file_group
 move_file ".vimrc", file_owner, file_group
 move_file ".tmux.conf", file_owner, file_group
+
+bash "install_vundle" do
+	user "#{file_owner}"
+	cwd "/home/#{file_owner}"
+	code <<-EOH
+	if [ ! -d /home/#{file_owner}/.vim/bundle ]; then
+	git clone https://github.com/gmarik/vundle.git /home/#{file_owner}/.vim/bundle/vundle
+	fi
+	EOH
+end
+
+bash "install_colors" do
+	user "#{file_owner}"
+	cwd "/home/#{file_owner}"
+
+	code <<-EOH
+	if [ ! -d /home/#{file_owner}/.vim/colors  ]; then
+	git clone https://github.com/chriskempson/tomorrow-theme /home/#{file_owner}/.vim/tomorrow-theme
+
+	mv /home/#{file_owner}/.vim/tomorrow-theme/vim/colors /home/#{file_owner}/.vim
+	sudo cp /home/#{file_owner}/.vim/colors/* /usr/share/vim/vimcurrent/colors
+
+	rm -rf /home/#{file_owner}/.vim/tomorrow-theme
+	rm -rf /home/#{file_owner}/.vim/colors
+	fi
+	EOH
+end
+
+bash "install_tmux_powerline" do
+	user "#{file_owner}"
+	cwd "/home/#{file_owner}"
+	
+	code <<-EOH
+	if [ ! -d /home/#{file_owner}/.tmux_powerline ]
+	git clone git://github.com/erikw/tmux-powerline.git /home/#{file_owner}/.tmux_powerline
+	fi
+	EOH
+end
+
+bash "bundle_install" do
+	user "#{file_owner}"
+	cwd "/home/#{file_owner}"
+
+	code <<-EOH
+	vim +BundleInstall
+	EOH
+end
+
+bash "configure_git" do
+	user "#{file_owner}"
+	cwd "/home/#{file_owner}"
+
+	code <<-EOH
+	git config --global user.name "#{git_name}"
+	git config --global user.email "#{git_email}"
+	git config --global credential.helper "cache --timeout=#{git_cache_creds}"
+	git config --global grep.linenumber 'true'
+	git config --global grep.extendedRegExp 'true'
+	EOH
+end
