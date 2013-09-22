ec114ca1a192b003e4890b5b6c0ed1c100bd071f
Cody Poll <CJPoll@gmail.com>
2013-09-22 00:22:00 -0600

    Testing if a URL can be added for a dependency.

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'cjpoll@gmail.com>'
 license          'All rights reserved'
 description      'Installs/Configures devenvset'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.12'
+version          '0.1.13'
 
 depends "build-essential"
 depends "cmake"
-depends "vim_chef"
+depends "vim_chef", git: "https://github.com/CJPoll/vim_chef.git"
