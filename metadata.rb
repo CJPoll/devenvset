a25858969f2f57651f4d8194c4e340740b1b7f2c
Cody Poll <CJPoll@gmail.com>
2013-09-23 15:39:12 -0600

    Removed dependency on vim_chef

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,4 @@ maintainer_email 'cjpoll@gmail.com>'
 license          'All rights reserved'
 description      'Installs/Configures devenvset'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.15'
-
-depends "vim_chef", git: "https://github.com/CJPoll/vim_chef.git"
+version          '0.1.16'
