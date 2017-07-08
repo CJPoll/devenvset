da5a9439dcd0867a5ca0d4c86f10623dce93f2f0
Cody Poll <cjpoll@gmail.com>
2017-07-08 14:48:16 -0600

    Add elixir proj

---
+++ lib/devenvset.ex
@@ -0,0 +1,18 @@
+defmodule Devenvset do
+  @moduledoc """
+  Documentation for Devenvset.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Devenvset.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/devenvset_test.exs
@@ -0,0 +1,8 @@
+defmodule DevenvsetTest do
+  use ExUnit.Case
+  doctest Devenvset
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
