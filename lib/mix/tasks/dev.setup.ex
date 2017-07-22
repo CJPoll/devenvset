fe9c40c00fac5e9978bd21cf02d286d2c5a4b3bc
Cody Poll <cjpoll@gmail.com>
2017-07-22 01:40:26 -0600

    Start devenvset in mix task

--- lib/mix/tasks/dev.setup.ex
+++ lib/mix/tasks/dev.setup.ex
@@ -4,6 +4,7 @@ defmodule Mix.Tasks.Dev.Setup do
   alias Coach.Cmd
 
   def run(_) do
+    Application.ensure_all_started(:devenvset)
     Devenvset.Setup.copy_dotfiles(run_as: "cjpoll") |> Cmd.run
   end
 end
