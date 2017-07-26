d482cdc0b04d5250c9d7003cfda9b471d1a3e22f
Cody Poll <cjpoll@gmail.com>
2017-07-25 23:01:45 -0600

    Shell setup
    1. Install oh-my-zsh as dev user
    2. Copy dotfiles after oh-my-zsh installed

--- lib/mix/tasks/dev.setup.ex
+++ lib/mix/tasks/dev.setup.ex
@@ -7,9 +7,9 @@ defmodule Mix.Tasks.Dev.Setup do
     Application.ensure_all_started(:devenvset)
 
     Devenvset.Setup.setup_user() |> Cmd.run
-    Devenvset.Setup.copy_dotfiles() |> Cmd.run
     Devenvset.Setup.install_infrastructure() |> Cmd.run
     Devenvset.Setup.setup_postgres() |> Cmd.run
     Devenvset.Setup.tmux() |> Cmd.run
+    Devenvset.Setup.copy_dotfiles() |> Cmd.run
   end
 end
