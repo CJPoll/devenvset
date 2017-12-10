8d306f0521100de777a517f43ba08e34616f14e0
Cody Poll <cjpoll@gmail.com>
2017-12-10 00:57:50 -0700

    Simplify

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/env bash
 
+USERNAME="${1}";
+
+if [ -z "${USERNAME}" ]; then
+	USERNAME="dev";
+fi
+
 echo "THIS IS WORKING";
 
-source "${HOME}/.asdf/asdf.sh";
-
-ssh -o StrictHostKeyChecking=no -T git@github.com
+ssh -o StrictHostKeyChecking=no -T git@github.com;
 
 rm -rf devenvset;
 
@@ -12,6 +16,12 @@ git clone https://github.com/cjpoll/devenvset;
 
 cd devenvset;
 
-mix deps.get;
-mix compile;
-mix dev.setup;
+useradd -d /home/"${USERNAME}" -m "${USERNAME}";
+passwd -d "${USERNAME}"
+
+cp "dotfiles/.zshrc" "/home/${USERNAME}/";
+cp "dotfiles/.tmux.conf" "/home/${USERNAME}/";
+cp "dotfiles/.vimrc" "/home/${USERNAME}/";
+cp "${HOME}/.ssh/authorized_keys" "/home/${USERNAME}/.ssh/authorized_keys"
+
+apk add sudo build-base zsh libevent-dev-2.1.8-r2 curl tmux
--- test/devenvset_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule DevenvsetTest do
-  use ExUnit.Case
-  doctest Devenvset
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
