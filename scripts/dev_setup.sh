cc777ef1f1fc74c24d966da7e167732f5205a2b2
root <cjpoll@gmail.com>
2018-08-29 04:42:57 +0000

    Some cleanup to get this working on a raw debian

--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env zsh
+#! /usr/bin/env bash
 
 USERNAME="${1}";
 
@@ -8,19 +8,30 @@ fi
 
 HOME="/home/${USERNAME}"
 
-#useradd -d /home/"${USERNAME}" -m "${USERNAME}";
-#passwd -d "${USERNAME}"
+useradd -d /home/"${USERNAME}" -m "${USERNAME}";
+passwd -d "${USERNAME}"
+
+mkdir -p "${HOME}/.ssh/";
 
 cp "dotfiles/.zshrc" "${HOME}";
 cp "dotfiles/.tmux.conf" "${HOME}";
 cp "dotfiles/.vimrc" "${HOME}";
 cp "dotfiles/.gitignore" "${HOME}";
-#cp "${HOME}/.ssh/authorized_keys" "${HOME}"
+cp ~root/.ssh/authorized_keys "${HOME}/.ssh/authorized_keys"
+
+#apt-get update;
+#apt-get install -y build-essential vim zsh curl tmux
 
 #apk add sudo build-base zsh curl tmux ncurses
 
-git clone "https://github.com/VundleVim/Vundle.vim.git" "${HOME}/.vim/bundle/Vundle.vim"
-su - "${USERNAME}" -c vim +PluginInstall +qall
+git clone https://github.com/cjpoll/custom "${HOME}/custom"
+
+touch "${HOME}/.vimrc.local";
+touch "${HOME}/.vimrc.local.plugins";
+git clone "https://github.com/VundleVim/Vundle.vim.git" "${HOME}/.vim/bundle/Vundle.vim";
+
+chown -R "${USERNAME}" "${HOME}/";
+su - "${USERNAME}" -c "vim +PluginInstall +qall";
 
 git clone git@github.com:robbyrussell/oh-my-zsh "${HOME}/.oh-my-zsh"
 git clone git@github.com:tmux-plugins/tpm "${HOME}/.tmux/plugins/tpm"
@@ -30,6 +41,9 @@ git config --global user.email "cjpoll@gmail.com"
 git config --global user.username "Cody J. Poll"
 git config --global core.excludesfile "${HOME}/.gitignore"
 
-chown -R "${USERNAME}" "${HOME}/.oh-my-zsh"
-chown -R "${USERNAME}" "${HOME}/.tmux/plugins/tpm"
-chown -R "${USERNAME}" "${HOME}/custom"
+chown -R "${USERNAME}" "${HOME}";
+chown -R "${USERNAME}" "${HOME}/.oh-my-zsh";
+chown -R "${USERNAME}" "${HOME}/.tmux/plugins/tpm";
+chown -R "${USERNAME}" "${HOME}/custom";
+
+chsh -s $(which zsh) "${USERNAME}"
